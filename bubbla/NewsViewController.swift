6a84fc03ead3ddef12850dfaadaa48fc7f3caab3
sami purmonen <purmonen@kth.se>
2015-10-21 15:21:31 +0200

    Added loading indicator to web view

--- bubbla/NewsViewController.swift
+++ bubbla/NewsViewController.swift
@@ -1,13 +1,21 @@
 import UIKit
 
-class NewsViewController: UIViewController {
+class NewsViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet weak var webView: UIWebView!
     
     var newsItem: BubblaNews!
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        NSOperationQueue.mainQueue().addOperationWithBlock {
+            self.view.stopActivityIndicator()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.startActivityIndicator()
+        webView.delegate = self
         webView.loadRequest(NSURLRequest(URL: newsItem.url))
     }
 }
